2e8a3d256a736a62e4a9549cb8e55684ad43d47a
Kingpin007 <anirudhkanabar@gmail.com>
2015-12-09 23:15:38 +0530

    Merge Sort - 1.1 Completed writing the mergeSort() function there are some errors related to the odd even numbers regarding the splitting of arrays leading to improper sorting as show in this commit. The print function has been introduced for testing the progrss of the array throughout the entire process of sorting.
    More detailed debuggin will be dealt with later on.

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void mergeSort(int *a, int n); //This function will split the array
 void merge(int *a,int *b, int *c,int n, int m);  //this function will merge
-
+void print(int *a,int n)
+{
+    int i=0;
+    printf("\n---------------\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",*(a+i));
+    }
+    printf("\n---------------\n");
+}
 int main()
 {
-  int a[10] = {9,6,3,5,8,1,2,4,7,0};
+  int a[10] = {9,6,3,5,8,1,2,4,7,10};
+  mergeSort(a,10);
   int i=0;
   for(i=0;i<10;i++)
-    printf("%d ", c[i]);
+    printf("%d ", a[i]);
   return 0;
 }
 
@@ -43,3 +54,35 @@ void merge(int *a, int *b,int *c,int n,int m) //merge function tested
     k++;
   }
 }
+
+void mergeSort(int *a,int n)  // n is the length of a
+{
+  //basic login - split arrays into smaller arrays recursively
+  if(n<2)
+    return;
+  int i=0;
+  int *x = malloc(2*n);
+  int *y = malloc(2*n);
+  for(i=0;i<n/2;i++)
+  {
+    *(x+i) = *(a+i);
+  }
+  int j=0;
+  while(i<n)
+  {
+    *(y+j) = *(a+i);
+    i++;
+    j++;
+  }
+  print(x,n/2);
+  mergeSort(x,n/2);
+  print(x,n/2);
+  print(y,n/2);
+  mergeSort(y,n/2);
+  print(y,n/2);
+  print(a,n);
+  merge(x,y,a,n/2,n/2);
+  print(a,n);
+  free(x);
+  free(y);
+}//some error related to the odd even case is occuring
